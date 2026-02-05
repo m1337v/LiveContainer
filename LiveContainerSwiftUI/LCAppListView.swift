85d0c7a100553defe93fec55af4ce3f0d4c768c2
khanhduytran0 <khanhduytran0@users.noreply.github.com>
2026-02-05 15:27:14 +0700

    Handle StikJITLC for multitask JIT

--- LiveContainerSwiftUI/LCAppListView.swift
+++ LiveContainerSwiftUI/LCAppListView.swift
@@ -1061,19 +1061,25 @@ struct LCAppListView : View, LCAppBannerDelegate, LCAppModelDelegate {
 
     }
     
-    func jitLaunch(withPID pid: Int) async {
+    func jitLaunch(withPID pid: Int, withScript script: String? = nil) async {
         await MainActor.run {
-            if let url = URL(string: "stikjit://enable-jit?bundle-id=\(Bundle.main.bundleIdentifier!)pid=\(pid)") {
-                UIApplication.shared.open(url)
-            }
-        }
-    }
-
-    func jitLaunch(withPID pid: Int, withScript script: String) async {
-        await MainActor.run {
-            let encoded = script.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? ""
-            if let url = URL(string: "stikjit://enable-jit?bundle-id=\(Bundle.main.bundleIdentifier!)&pid=\(pid)&script-data=\(encoded)") {
-                UIApplication.shared.open(url)
+            let encoded = script?.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
+                .map { "&script-data=\($0)" } ?? ""
+            if let url = URL(string: "stikjit://enable-jit?bundle-id=\(Bundle.main.bundleIdentifier!)&pid=\(pid)\(encoded)") {
+                if let jitEnabler = JITEnablerType(rawValue: LCUtils.appGroupUserDefault.integer(forKey: "LCJITEnablerType")), jitEnabler == .StikJITLC {
+                    if let app = sharedModel.apps.first(where: { app in
+                        return app.appInfo.urlSchemes().contains("stikjit") &&
+                        (sharedModel.multiLCStatus != 2 || app.appInfo.isShared)
+                    }) {
+                        Task { await openWebView(urlString: url.absoluteString) }
+                    } else {
+                        errorInfo = "StikDebug is not found. Please install it first and switch it to shared app."
+                        errorShow = true
+                        return
+                    }
+                } else {
+                    UIApplication.shared.open(url)
+                }
             }
         }
     }
