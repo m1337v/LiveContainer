m1337v/LiveContainer

commit 99d61e070d33dd712a9c24c05793f3480c867baf

Disable game mode by default since the banner is really annoying and cannot be dismissed in iOS 26.1

LiveContainerSwiftUI/LCMultiLCManagementView.swift
@@ -50,8 +50,8 @@ struct InstallAnotherLCButton : View {
 }
 
 struct LCMultiLCManagementView : View, InstallAnotherLCButtonDelegate {
-    @AppStorage("LCMultiAllowGameCategory") var useGameCategory = true
-    @AppStorage("LCMultiAllowGameMode") var allowGameMode = true
+    @AppStorage("LCMultiAllowGameCategory") var useGameCategory = false
+    @AppStorage("LCMultiAllowGameMode") var allowGameMode = false
     @State var errorShow = false
     @State var errorInfo = ""
     @State var successShow = false

Resources/Info.plist
@@ -34,7 +34,7 @@
 	<key>CFBundlePackageType</key>
 	<string>APPL</string>
 	<key>CFBundleShortVersionString</key>
-	<string>3.6.57</string>
+	<string>3.6.58</string>
 	<key>CFBundleSignature</key>
 	<string>????</string>
 	<key>CFBundleURLTypes</key>
@@ -49,7 +49,7 @@
 		</dict>
 	</array>
 	<key>CFBundleVersion</key>
-	<string>3.6.57</string>
+	<string>3.6.58</string>
 	<key>GCSupportedGameControllers</key>
 	<array>
 		<dict>
@@ -67,10 +67,8 @@
 	</array>
 	<key>GCSupportsControllerUserInteraction</key>
 	<true/>
-	<key>GCSupportsGameMode</key>
-	<true/>
 	<key>LSApplicationCategoryType</key>
-	<string>public.app-category.games</string>
+	<string>public.app-category.education</string>
 	<key>LSApplicationQueriesSchemes</key>
 	<array>
 		<string>sidestore</string>

commit 84770381c032f792c5c775fe347e2e3cb742691c

Fix #1150
Supplement change for 99d61e0 which introduced a breaking change in Info.plist but it was left unnoticed

LiveContainerSwiftUI/LCMultiLCManagementView.swift
@@ -118,14 +118,15 @@ struct LCMultiLCManagementView : View, InstallAnotherLCButtonDelegate {
         }
         
         do {
-            var keysToExclude: [String] = []
-            if !useGameCategory {
-                keysToExclude.append("LSApplicationCategoryType")
+            var extraInfo: [String : Any] = [:]
+            if useGameCategory {
+                extraInfo["LSApplicationCategoryType"] = "public.app-category.games"
             }
-            if !allowGameMode {
-                keysToExclude.append(contentsOf: ["GCSupportsGameMode", "LSSupportsGameMode"])
+            if allowGameMode {
+                extraInfo["GCSupportsGameMode"] = true
+                extraInfo["LSSupportsGameMode"] = true
             }
-            let packedIpaUrl = try LCUtils.archiveIPA(withBundleName: name, excludingInfoPlistKeys: keysToExclude)
+            let packedIpaUrl = try LCUtils.archiveIPA(withBundleName: name, includingExtraInfoDict: extraInfo)
             
             shareURL = packedIpaUrl
             

LiveContainerSwiftUI/LCUtils.h
@@ -26,7 +26,7 @@ int dyld_get_program_sdk_version(void);
 @interface LCUtils : NSObject
 
 + (void)validateJITLessSetupWithCompletionHandler:(void (^)(BOOL success, NSError *error))completionHandler;
-+ (NSURL *)archiveIPAWithBundleName:(NSString*)newBundleName excludingInfoPlistKeys:(NSArray *)keysToExclude error:(NSError **)error;
++ (NSURL *)archiveIPAWithBundleName:(NSString*)newBundleName includingExtraInfoDict:(NSDictionary *)extraInfoDict error:(NSError **)error;
 + (NSData *)certificateData;
 + (void)launchMultitaskGuestApp:(NSString *)displayName completionHandler:(void (^)(NSError *error))completionHandler API_AVAILABLE(ios(16.0));
 + (void)launchMultitaskGuestAppWithPIDCallback:(NSString *)displayName pidCompletionHandler:(void (^)(NSNumber *pid, NSError *error))completionHandler API_AVAILABLE(ios(16.0));

LiveContainerSwiftUI/LCUtils.m
@@ -297,7 +297,7 @@
 
 }
 
-+ (NSURL *)archiveIPAWithBundleName:(NSString*)newBundleName excludingInfoPlistKeys:(NSArray *)keysToExclude error:(NSError **)error {
++ (NSURL *)archiveIPAWithBundleName:(NSString*)newBundleName includingExtraInfoDict:(NSDictionary *)extraInfoDict error:(NSError **)error {
     if (*error) return nil;
 
     NSFileManager *manager = NSFileManager.defaultManager;
@@ -339,7 +339,7 @@
     }
     infoDict[@"CFBundleIcons~ipad"][@"CFBundlePrimaryIcon"][@"CFBundleIconFiles"][0] = @"AppIconGrey60x60";
     infoDict[@"CFBundleIcons~ipad"][@"CFBundlePrimaryIcon"][@"CFBundleIconFiles"][1] = @"AppIconGrey76x76";
-    [infoDict removeObjectsForKeys:keysToExclude];
+    [infoDict addEntriesFromDictionary:extraInfoDict];
     
     // reset a executable name so they don't look the same on the log
     NSURL* appBundlePath = [tmpPayloadPath URLByAppendingPathComponent:@"App.app"];